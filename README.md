a4f08ddeea13bc595186715ab839046fb63b7edf
Giles Barford <giles.barford@gmail.com>
2018-04-26 07:04:03 +0000

    add readme

---
+++ README.md
@@ -0,0 +1,31 @@
+Net2Host
+========
+
+You will need redis on your host and Bro
+
+Conf directory Files:
+=====================
+
+net2Host.conf:
+ Net2Host Configuration
+ File Paths for Log files ETC
+
+nxlog-linux-config:
+ Configuration for recieving NX log information from windows.   Should be possible to use syslog instead if you wish.
+
+nxlog-window-config:
+ NX log configuration for Windows.   This takes windows events and forwards them to windows.
+
+
+Processes
+==========
+ bro-ssl-read.py: This read data from bro SSL logs and populates redis database
+                ./bro-ssl-read.py start
+
+ host-read.py:  This reads data from file created by NXlog on linux machine and populates Redis data
+                ./host-read.py start
+
+ correlate.py:   This checks redis database and correlates logs. Still a more work to do here.
+
+ process-stdin.py:  Use this utility to run daemon process and except input on standard input.  
+                    e.g.: ./process-stdin bro-ssl-read
--- correlate.py
+++ correlate.py
@@ -7,21 +7,27 @@ import sys
 import ipaddress
 import redis
 import time
+from helperFunctions import *
 
 def errorLog(errorMsg):
     print(errorMsg)
 
 rd = redis.Redis()
 
-while True:
+configuration=readConfigToDict(os.path.basename(__file__).split(".")[0])
+
+
+with open(configuration[u'jsonoutfile'], 'w') as jsonOut:
+
+  while True:
     if rd.llen('toProcess') > 0:
         key=rd.rpop('toProcess')
         if rd.exists(key):
             if rd.hexists(key,"hostApp") and rd.hexists(key,"ja3"):
                 app=rd.hget(key,"hostApp")
                 ja3=rd.hget(key,"ja3")
-                print("JA3: " + ja3 + " app: " + app + " src: " + rd.hget(key,"id.orig_h") \
-                          + " dst: " + rd.hget(key,"id.resp_h"))
+                json.dump(rd.hgetall(key),jsonOut)
+                jsonOut.write("\n")
                 if rd.exists(app):
                     if not rd.sismember(app,ja3):
                         print("New JA3: " + ja3 + " for app: " + app + " src: " + rd.hget(key,"id.orig_h") \

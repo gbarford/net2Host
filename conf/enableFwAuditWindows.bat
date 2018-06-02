2a566375c0b61a0a34495c6dc71408e66800596e
root <root@docker1>
2018-06-02 09:07:36 +0000

    add windows auditing commands

---
+++ conf/enableFwAuditWindows.bat
@@ -0,0 +1,2 @@
+Auditpol /set /category:"System" /SubCategory:"Filtering Platform Packet Drop" /success:enable /failure:enable
+Auditpol /set /category:"System" /SubCategory:"Filtering Platform Connection" /success:enable /failure:enable
